bf2ad447c8578d9aee812a4df420747818104bea
it21296796 <99311998+it21296796@users.noreply.github.com>
2024-05-06 01:03:33 +0530

    working on Payment gateway

--- Admin_Service/index.js
+++
@@ -1,2 +0,0 @@
-//gghggg
-//adgadhgdaghd
---
+++ Course-Service/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cors = require('cors')
+
+const app = express();
+const PORT = process.env.PORT ;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+app.use(cors())
+app.use(express.json())  
+
+mongoose.connect(MONGODB_URI)
+   .then(() => console.log('DB connected succsessfully'))
+   .catch(err => console.log(err));
+
+
+   //Start server
+
+app.listen(PORT , () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+app.use('/' ,(req,res)=>{
+    res.json('Course Service')
+})
