6f81d4f8f93065d2c7ed1c3fc7dac9e2007b1c8c
Malith Jayasinghe <malith2jayasinghe@gmail.com>
2024-05-12 18:42:46 +0530

    uopdate app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,56 +30,65 @@ import DisplayRequests from "./Pages/Dashboard/DisplayRequests";
 import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { Navigate } from 'react-router-dom';
 
 import TeacherRoute from './Components/TeacherRoute'
 import AdminRoute from './Components/AdminRoute'
 import UserRoute from './Components/UserRoute'
 
 function App() {
-  const {userInfo} = useSelector((state) => state.signIn) 
-  console.log('hi' , userInfo)
+  const { userInfo } = useSelector((state) => state.signIn)
+  console.log('hi', userInfo)
   return (
     <>
-    <ToastContainer />
-    {/* <CssBaseline /> */}
-                
-                
+      <ToastContainer />
+      {/* <CssBaseline /> */}
+
+
       <Routes>
-      
+
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/denied" element={<Denied />} />
+        {userInfo ? (
+          <Route path="/signup" element={<Navigate to="/" />} />
+        ) :
+          (<Route path="/signup" element={<Signup />} />
+          )}
 
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        
-        
+        {userInfo ? (
+          <Route path="/login" element={<Navigate to="/" />} />
+        ) :
+          (<Route path="/login" element={<Login />} />
+          )}
+
+
 
         <Route path="/courses" element={<CourseList />} />
         <Route path="/courses/description" element={<CourseDescription />} />
 
-        
-          <Route path="/course/create" element={<TeacherRoute><CreateCourse /></TeacherRoute>} />
-          <Route path="/course/addlecture" element={<AddLecture />} />
-          <Route path="/admin/dashboard" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
-          <Route path="/teacher/dashboard" element={<TeacherDashboard />} />
-          <Route path="/admin/requests" element={<DisplayRequests />} />
-         
-        
 
-        
-          <Route path="/user/profile" element={<Profile />} />
-          
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/checkout/success" element={<CheckoutSuccess />} />
-          <Route path="/checkout/fail" element={<CheckoutFail />} />
-          <Route path="/course/displaylectures" element={<DisplayLecture />} />
-        
+        <Route path="/course/create" element={<TeacherRoute><CreateCourse /></TeacherRoute>} />
+        <Route path="/course/addlecture" element={<TeacherRoute><AddLecture /></TeacherRoute>} />
+        <Route path="/admin/dashboard" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
+        <Route path="/teacher/dashboard" element={<TeacherRoute><TeacherDashboard /></TeacherRoute>} />
+        <Route path="/admin/requests" element={<AdminRoute><DisplayRequests /></AdminRoute>} />
+
+
+
+
+        <Route path="/user/profile" element={<Profile />} />
+
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/checkout/success" element={<CheckoutSuccess />} />
+        <Route path="/checkout/fail" element={<CheckoutFail />} />
+        <Route path="/course/displaylectures" element={<DisplayLecture />} />
+
 
         <Route path="*" element={<NotFound />} />
       </Routes>
-    
+
     </>
   );
 }
